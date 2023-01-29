6eed5e60933d6420bfc1a8f49d766d36905442a1
PhoenixAssutech <amadu@assutech.gm>
2023-01-29 10:48:12 +0000

    Working from both computers

--- ServerSocket.py
+++ ServerSocket.py
@@ -4,11 +4,11 @@ import socket
 def server_program():
     # get the hostname
     host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
+    port = 5002  # initiate port no above 1024
 
-    server_socket = socket.socket()  # get instance
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # get instance
     # look closely. The bind() function takes tuple as argument
-    server_socket.bind((host, port))  # bind host address and port together
+    server_socket.bind(('192.168.0.108', port))  # bind host address and port together
 
     # configure how many client the server can listen simultaneously
     server_socket.listen(2)
